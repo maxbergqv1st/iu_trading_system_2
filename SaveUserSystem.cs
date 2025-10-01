f953e368dffbcb14a45aa4072a4de4cf5e27c56f
unknown <mjmaxbq@icloud.com>
2025-10-01 11:17:25 +0200

    added LoadUser() for loading users.txt

--- SaveUserSystem.cs
+++ SaveUserSystem.cs
@@ -4,7 +4,6 @@ namespace App;
 
 public class SaveUserSystem
 {
-
       public void SaveUser(List<IUser> users)
       {
             // string[] lines = new string[users.Count];
@@ -14,5 +13,22 @@ public class SaveUserSystem
                   lines.Add($"{acc.Name},{acc.Username},{acc._password}");
             }
             File.WriteAllLines("users.txt", lines);
+      }
+      public List<IUser> LoadUser() // void doesnt return anny value
+      {
+            List<IUser> users = new List<IUser>();
+            string[] lines = File.ReadAllLines("users.txt");
+            foreach (string line in lines)
+            {
+                  string[] split = line.Split(",");
+                  if (split.Length == 3)
+                  {
+                        string name = split[0];
+                        string username = split[1];
+                        string password = split[2];
+                        users.Add(new Account(name, username, password));
+                  }
+            }
+            return users;
       }
 }
