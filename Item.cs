6cb0da308801922c5e8e190f71b074b677752443
unknown <mjmaxbq@icloud.com>
2025-10-01 13:06:03 +0200

    changed owner to be in top of strings arg in item.cs

--- Item.cs
+++ Item.cs
@@ -2,14 +2,15 @@ namespace App;
 
 public class Item
 {
+      public string OwnerUsername { get; set; }
       public string Name { get; set; }
       public string Description { get; set; }
-      public string OwnerUsername { get; set; }
+      
 
-      public Item(string name, string description, string ownerUsername)
+      public Item(string ownerUsername, string name, string description)
       {
+            OwnerUsername = ownerUsername;
             Name = name;
             Description = description;
-            OwnerUsername = ownerUsername;
       }
 }
