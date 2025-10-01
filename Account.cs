4fc1800b08ca633dc1e8ed478b58cae8adeeccc6
unknown <mjmaxbq@icloud.com>
2025-10-01 10:42:53 +0200

    added Account.cs

---
+++ Account.cs
@@ -0,0 +1,21 @@
+namespace App;
+
+class Account : IUser
+{
+      public string Name;
+      public string Username;
+
+      string _password;
+
+      public Account(string name, string username, string password)
+      {
+            Name = name;
+            Username = username;
+            _password = password;
+      }
+
+      public bool TryLogin(string username, string password)
+      {
+            return username == Username && password == _password;
+      }
+}
